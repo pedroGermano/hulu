500c50891ed7dd41569756943988bfed20f12dc1
Pedro Germano <pedrogermano232@gmail.com>
2022-10-29 13:22:17 -0300

    Header

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+import Header from '../components/Header'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Hulu 2.0</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Header />
+    </div>
+  )
+}
